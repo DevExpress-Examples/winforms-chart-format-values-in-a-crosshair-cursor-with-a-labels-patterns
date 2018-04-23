0c5961d105304c5f3532e624fcd6aca12c64a026
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to format values in a crosshair cursor via a label's patterns
+
+
+<p>This example  demonstrates changing a text displayed  in crosshair labels using crosshair format patterns. </p><p>To accomplish this, it is necessary to specify a string which will represent a displayed pattern within a crosshair label using the  <a href="http://documentation.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairAxisLabelOptions_Patterntopic"><u>CrosshairAxisLabelOptions.Pattern</u></a>, <a href="http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsSeriesBase_CrosshairLabelPatterntopic"><u>SeriesBase.CrosshairLabelPattern</u></a>, and <a href="http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_GroupHeaderPatterntopic"><u>CrosshairOptions.GroupHeaderPattern</u></a> properties.</p><p>In addition, standard and custom format specifiers are used together with the placeholders to format numeric and date/time values (e.g., "<strong>{A:F0}</strong>"). To learn more, see the <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument2141"><u>Format Specifiers</u></a> topic. <br />
+</p><p>To learn more on a crosshair cursor, see the <a href="http://help.devexpress.com/#XtraCharts/CustomDocument11976"><u>Crosshair Cursor</u></a>  topic. </p><br />
+
+
+<br/>
+
+
---
+++ VB/FormatCrosshairLabels/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+
+Namespace FormatCrosshairLabels
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			AddHandler Load, AddressOf Form1_Load
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+			' Create an empty chart.
+			Dim chartControl1 As New ChartControl()
+
+			' Create a first line series and add points to it.
+			Dim series1 As New Series("Europe", ViewType.Line)
+			series1.Points.Add(New SeriesPoint(1950, 546))
+			series1.Points.Add(New SeriesPoint(1960, 605))
+			series1.Points.Add(New SeriesPoint(1970, 656))
+			series1.Points.Add(New SeriesPoint(1980, 694))
+			series1.Points.Add(New SeriesPoint(1990, 721))
+			series1.Points.Add(New SeriesPoint(2000, 730))
+			series1.Points.Add(New SeriesPoint(2010, 728))
+
+			' Create a second line series and add points to it.
+			Dim series2 As New Series("Americas", ViewType.Line)
+			series2.Points.Add(New SeriesPoint(1950, 332))
+			series2.Points.Add(New SeriesPoint(1960, 417))
+			series2.Points.Add(New SeriesPoint(1970, 513))
+			series2.Points.Add(New SeriesPoint(1980, 614))
+			series2.Points.Add(New SeriesPoint(1990, 721))
+			series2.Points.Add(New SeriesPoint(2000, 836))
+			series2.Points.Add(New SeriesPoint(2010, 935))
+
+			' Add the series to the chart.
+			chartControl1.Series.Add(series1)
+			chartControl1.Series.Add(series2)
+
+			' Hide the legend (if necessary).
+			chartControl1.Legend.Visible = False
+
+			'Show crosshair axis lines and axis labels to see format values of crosshair labers 
+			chartControl1.CrosshairOptions.ShowArgumentLabels = True
+			chartControl1.CrosshairOptions.ShowValueLabels = True
+			chartControl1.CrosshairOptions.ShowValueLine = True
+			chartControl1.CrosshairOptions.ShowArgumentLine = True
+
+			' Specify the crosshair label pattern.
+			series1.CrosshairLabelPattern = "{S} population: {V}"
+			series2.CrosshairLabelPattern = "{S} population: {V}"
+
+			' Specify the crosshair group header pattern.
+			chartControl1.CrosshairOptions.GroupHeaderPattern = "Year: {A}"
+
+			' Cast the chart's diagram to the XYDiagram type, to access its axes.
+			Dim diagram As XYDiagram = CType(chartControl1.Diagram, XYDiagram)
+
+			' Specify the crosshair axis labels pattern for X-axis.
+			diagram.AxisX.CrosshairAxisLabelOptions.Pattern = "Year: {A:F0}"
+
+			' Add the chart to the form.
+			chartControl1.Dock = DockStyle.Fill
+			Me.Controls.Add(chartControl1)
+		End Sub
+
+	End Class
+End Namespace
