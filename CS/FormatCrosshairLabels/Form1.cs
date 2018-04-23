a80fd44cd138bd7543660d4064d02c9e706b13d8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FormatCrosshairLabels/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+
+namespace FormatCrosshairLabels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create an empty chart.
+            ChartControl chartControl1 = new ChartControl();
+
+            // Create a first line series and add points to it.
+            Series series1 = new Series("Europe", ViewType.Line);
+            series1.Points.Add(new SeriesPoint(1950, 546));
+            series1.Points.Add(new SeriesPoint(1960, 605));
+            series1.Points.Add(new SeriesPoint(1970, 656));
+            series1.Points.Add(new SeriesPoint(1980, 694));
+            series1.Points.Add(new SeriesPoint(1990, 721));
+            series1.Points.Add(new SeriesPoint(2000, 730));
+            series1.Points.Add(new SeriesPoint(2010, 728));
+
+            // Create a second line series and add points to it.
+            Series series2 = new Series("Americas", ViewType.Line);
+            series2.Points.Add(new SeriesPoint(1950, 332));
+            series2.Points.Add(new SeriesPoint(1960, 417));
+            series2.Points.Add(new SeriesPoint(1970, 513));
+            series2.Points.Add(new SeriesPoint(1980, 614));
+            series2.Points.Add(new SeriesPoint(1990, 721));
+            series2.Points.Add(new SeriesPoint(2000, 836));
+            series2.Points.Add(new SeriesPoint(2010, 935));
+
+            // Add the series to the chart.
+            chartControl1.Series.Add(series1);
+            chartControl1.Series.Add(series2);
+
+            // Hide the legend (if necessary).
+            chartControl1.Legend.Visible = false;
+
+            //Show crosshair axis lines and crosshair axis labels to see format values of crosshair labels 
+            chartControl1.CrosshairOptions.ShowArgumentLabels = true;
+            chartControl1.CrosshairOptions.ShowValueLabels = true;
+            chartControl1.CrosshairOptions.ShowValueLine = true;
+            chartControl1.CrosshairOptions.ShowArgumentLine = true;
+
+            // Specify the crosshair label pattern for both series.
+            series1.CrosshairLabelPattern = "{S} population: {V}";
+            series2.CrosshairLabelPattern = "{S} population: {V}";
+
+            // Specify the crosshair group header pattern.
+            chartControl1.CrosshairOptions.GroupHeaderPattern = "Year: {A}";
+
+            // Cast the chart's diagram to the XYDiagram type, to access its axes.
+            XYDiagram diagram = (XYDiagram)chartControl1.Diagram;
+
+            // Specify the crosshair axis labels pattern for X-axis.
+            diagram.AxisX.CrosshairAxisLabelOptions.Pattern = "Year: {A:F0}";
+
+            // Add the chart to the form.
+            chartControl1.Dock = DockStyle.Fill;
+            this.Controls.Add(chartControl1);
+        }
+
+    }
+}
---
+++ VB/FormatCrosshairLabels/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿Namespace FormatCrosshairLabels
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim sideBySideBarSeriesLabel2 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+            sideBySideBarSeriesLabel2.LineVisible = True
+            Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel2
+            Me.chartControl1.Size = New System.Drawing.Size(284, 262)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(284, 262)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+           'this.Load += new System.EventHandler(this.Form1_Load);
+            DirectCast(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+    End Class
+End Namespace
+
