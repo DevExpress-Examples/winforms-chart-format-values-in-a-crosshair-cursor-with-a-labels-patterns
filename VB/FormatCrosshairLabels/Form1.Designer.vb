0d4d4be493efa6d3a938b33060d874ae3d5e8461
DevExpressExampleBot <fakeEmail>
2023-12-12 15:40:13 +0400

    Source auto update [skip ci]

--- VB/FormatCrosshairLabels/Form1.Designer.vb
+++ VB/FormatCrosshairLabels/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace FormatCrosshairLabels
-    Partial Public Class Form1
+Namespace FormatCrosshairLabels
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,23 +12,23 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim sideBySideBarSeriesLabel2 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim sideBySideBarSeriesLabel2 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((sideBySideBarSeriesLabel2), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' chartControl1
@@ -34,7 +36,7 @@
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
             sideBySideBarSeriesLabel2.LineVisible = True
             Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel2
             Me.chartControl1.Size = New System.Drawing.Size(284, 262)
@@ -48,16 +50,13 @@
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-           'this.Load += new System.EventHandler(this.Form1_Load);
-            DirectCast(sideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            'this.Load += new System.EventHandler(this.Form1_Load);
+            CType((sideBySideBarSeriesLabel2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private chartControl1 As DevExpress.XtraCharts.ChartControl
     End Class
 End Namespace
-
--- VB/FormatCrosshairLabels/Form1.vb
+++ VB/FormatCrosshairLabels/Form1.vb
@@ -1,21 +1,21 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 
 Namespace FormatCrosshairLabels
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
-            AddHandler Load, AddressOf Form1_Load
+            AddHandler Load, New EventHandler(AddressOf Form1_Load)
         End Sub
 
         Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an empty chart.
-            Dim chartControl1 As New ChartControl()
-
+            Dim chartControl1 As ChartControl = New ChartControl()
             ' Create a first line series and add points to it.
-            Dim series1 As New Series("Europe", ViewType.Line)
+            Dim series1 As Series = New Series("Europe", ViewType.Line)
             series1.Points.Add(New SeriesPoint(1950, 546))
             series1.Points.Add(New SeriesPoint(1960, 605))
             series1.Points.Add(New SeriesPoint(1970, 656))
@@ -23,9 +23,8 @@ Namespace FormatCrosshairLabels
             series1.Points.Add(New SeriesPoint(1990, 721))
             series1.Points.Add(New SeriesPoint(2000, 730))
             series1.Points.Add(New SeriesPoint(2010, 728))
-
             ' Create a second line series and add points to it.
-            Dim series2 As New Series("Americas", ViewType.Line)
+            Dim series2 As Series = New Series("Americas", ViewType.Line)
             series2.Points.Add(New SeriesPoint(1950, 332))
             series2.Points.Add(New SeriesPoint(1960, 417))
             series2.Points.Add(New SeriesPoint(1970, 513))
@@ -33,37 +32,28 @@ Namespace FormatCrosshairLabels
             series2.Points.Add(New SeriesPoint(1990, 721))
             series2.Points.Add(New SeriesPoint(2000, 836))
             series2.Points.Add(New SeriesPoint(2010, 935))
-
             ' Add the series to the chart.
             chartControl1.Series.Add(series1)
             chartControl1.Series.Add(series2)
-
             ' Hide the legend (if necessary).
             chartControl1.Legend.Visible = False
-
             'Show crosshair axis lines and crosshair axis labels to see format values of crosshair labels 
             chartControl1.CrosshairOptions.ShowArgumentLabels = True
             chartControl1.CrosshairOptions.ShowValueLabels = True
             chartControl1.CrosshairOptions.ShowValueLine = True
             chartControl1.CrosshairOptions.ShowArgumentLine = True
-
             ' Specify the crosshair label pattern for both series.
             series1.CrosshairLabelPattern = "{S} population: {V}"
             series2.CrosshairLabelPattern = "{S} population: {V}"
-
             ' Specify the crosshair group header pattern.
             chartControl1.CrosshairOptions.GroupHeaderPattern = "Year: {A}"
-
             ' Cast the chart's diagram to the XYDiagram type, to access its axes.
             Dim diagram As XYDiagram = CType(chartControl1.Diagram, XYDiagram)
-
             ' Specify the crosshair axis labels pattern for X-axis.
             diagram.AxisX.CrosshairAxisLabelOptions.Pattern = "Year: {A:F0}"
-
             ' Add the chart to the form.
             chartControl1.Dock = DockStyle.Fill
             Me.Controls.Add(chartControl1)
         End Sub
-
     End Class
 End Namespace
